944ae66a84bfaa6702b28ff010ab0b012ee94c41
Gasanova Tamara <solas93@mail.ru>
2023-12-22 21:32:58 +0300

    feat: implement createDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  const membersArr = members.map((item) => {
+    if (typeof item !== 'string') {
+      return;
+    }
+    return item.replaceAll(' ', '');
+  }).filter(Boolean);
+  
+  const firstChars = membersArr.reduce((acc, item) => {
+    if (typeof item === 'string') {
+      return acc + item[0].toUpperCase(); 
+    }  
+  }, '');
+
+  return firstChars.split('').sort((a, b) => a.localeCompare(b)).join('');
 }
 
 module.exports = {
